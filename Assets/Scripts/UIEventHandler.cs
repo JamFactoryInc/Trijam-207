e8e74b95d9e6ec160a3751628029feaceeea0193
Kieran Smith <kieran.jas.smith@gmail.com>
2023-02-19 11:46:08 +1000

    Commit got dropped

---
+++ Assets/Scripts/UIEventHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEventHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        LemmingController.Pause();
+        PlaceableObject.placingEnabled = true;
+    }
+
+    public void Play()
+    {
+        LemmingController.Resume();
+        PlaceableObject.placingEnabled = false;
+    }
+}
